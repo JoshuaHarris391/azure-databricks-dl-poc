32c1cda904990851ae468adcc8af41ad7d9e350c
JoshuaHarris391 <harjo391@gmail.com>
2026-02-20 17:29:25 +1100

    feat: add Databricks workspace URL output for easy access

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -7,3 +7,8 @@ resource "azurerm_databricks_workspace" "this" {
 
   tags = azurerm_resource_group.this.tags
 }
+
+output "databricks_workspace_url" {
+  value = azurerm_databricks_workspace.this.workspace_url
+  description = "URL of the Databricks workspace"
+}
