77a3718449a94138d1642fb00ebb3e0120d29d0b
JoshuaHarris391 <harjo391@gmail.com>
2026-02-22 16:39:40 +1100

    feat: implement SHA-256 hashing for PHI fields in silver_hashed_phi model with Patient source data transformation

--- dbt_project/models/silver/silver_hashed_phi.sql
+++ dbt_project/models/silver/silver_hashed_phi.sql
@@ -3,3 +3,11 @@
         materialized='table'
     )
 }}
+
+SELECT DISTINCT
+    id,
+    sha2(family_name, 256) AS family_name_hashed,
+    sha2(given_names, 256) AS given_names_hashed,
+    birth_date
+FROM {{ source('bronze', 'patient') }}
+WHERE family_name IS NOT NULL
